8c0424e6ab01917809196d103409725445744cce
Chase Maxfield <maxfield.chase@gmail.com>
2023-02-28 22:52:34 -0700

    Finished February Creation

--- README.md
+++ README.md
@@ -1,25 +1,49 @@
-# IS 565 January Creation Project
+# IS 565 February Creation Project
 ## Team 3
 *Michael Bramwell, Nathan Gardner, Chase Maxfield, Nathan Moser, Devin Rockwood*
 
 ## Project
 
-For our project, we are creating a linux tool that gather security articles from around the interent using RSS Feeds and web scraping, it will then compile all the articles in a nice repository for easy viewing from security professionals.
+For our project, we are creating a linux tool that gather security articles from around the interent using RSS Feeds and web scraping. After gathering these security articles, it then uses topic analysis and text summary tools from hugging face to slim down the amount of news for busy cybersecurity professionals.
 
-Initially we are planning on it being commandline based for ease of initial development, but will introduce a GUI/web app later on.
+## Installation
 
-In addition, we hope to use a text summarizer tool and natural language processing to slim down the amount of news for busy cybersecurity professionals. 
+You will need the following python libraries in order to use this tool:
+- spacy
+- transformers (from hugging face)
+- feedparser
+- beautifulsoup4
+
+When this script runs for the first time, it will need to download a summarization model from hugging face, which it will do automatically. It will take ~1GiB of space.
 
 ## Using this tool
 
-Run the following command:
+This tool often requires several minutes to run to completion depending on your hardware, due to the heavy amount of machine learning it uses. Run the following command and let it run to completion:
 ```
 python news_summarizer.py
 ```
 
+Also included in the repo are two iterations of .ipynb we used to piecewise assemble the summarizers, including a first attempt. They are not necessary to run the tool; they simply exist to show some of the development process.
+
 ## How it works so far
 
-The tool first queries several of the top RSS feeds for cybersecurity news using the feedparser library. It then extracts the links from each of the articles it has received in the feed and fetches the actual web pages containing those articles. From each of those articles, it extracts the readable body text by identifying the p tags on those web pages. Finally, it compiles all of those articles into one output for the reader.
+The tool first queries several of the top RSS feeds for cybersecurity news using the feedparser library. It then extracts the links from each of the articles it has received in the feed and fetches the actual web pages containing those articles. From each of those articles, it extracts the readable body text by identifying the p tags on those web pages. 
+
+The tool then extracts all of the proper nouns from each article that occur at least once and likely several times. It then compares each news source's top proper nouns to see which proper nouns appear across the most news sources, hopefully identifying the most important news topics of the day. It then summarizes all of the news article that contain those proper nouns by first performing extractive summarization, which identifies the top sentences in each
+
+## Actual news summaries from the tool
+
+Date: 2/28/2023
+
+Topic: Ukraine 
+
+Russia's cyber attacks against Ukraine surged by 250% in 2022 when compared to two years ago, Google's Threat Analysis Group and Mandiant disclosed in a new joint report . Russia’s military intelligence service, the GRU, “launched destructive wiper attacks on hundreds of systems in Ukrainian government, IT, energy, and financial organizations,” Microsoft said . Despite Russia's conventional military setbacks and its failure to substantively advance its agenda through cyber operations, Russia maintains its intent to bring Ukraine under Russian control .
+
+Topic: Windows 
+
+January brings 10 critical updates as well as 67 patches rated as important to the Windows platform . With Windows 10 21H2 now out of mainstream support, we have the following Microsoft applications that will reach end of support or servicing in 2023 . With all of these more difficult testing scenarios, we recommend that you scan your application portfolio for updated application components or system-level dependencies . Given the large number of changes included this month, I have broken down the testing scenarios into high risk and standard risk groups .
+
+Obviously, these summaries are not perfect, but given a larger transformer model, the outputs could probably improve significantly. Future versions of this tool could use the GPT-3 API to summarize the articles.
 
 ## Usefulness of this tool
 
@@ -32,6 +56,9 @@ The tool first queries several of the top RSS feeds for cybersecurity news using
 Cybersecurity professionals need to stay curent with cybersecurity news because new exploits and vulnerabilities are developed all the time. They run the risk of not discovering vulnerabilities in their own systems and major threats if they are not aware of the threat landscape that faces them. This tool will help cybersecurity professionals stay up to date with these threats while minimizing the amount of time that they'll need to research the threats. 
 
 ## References
+
 Tools used:
 - https://jcutrer.com/python/python-tutorial-howto-parse-rss-headlines
 - https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+- https://www.activestate.com/blog/how-to-do-text-summarization-with-python/
+- https://stackoverflow.com/questions/3173320/text-progress-bar-in-terminal-with-block-characters
--- news_summarizer.py
+++ news_summarizer.py
@@ -1,7 +1,100 @@
+
+############################### IMPORTS ###############################
+
+
+# These imports and modifications are necessary for the two summarizer functions
+
+import spacy
+from spacy.lang.en.stop_words import STOP_WORDS
+from string import punctuation
+
+from transformers import pipeline
+
+updated_punctuation = punctuation + "”"
+
+# These imports are necessary for the HTML parser to work
 import feedparser
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 import urllib.request
+
+
+############################## FUNCTIONS ###############################
+
+
+# This function takes in a collection of articles from each news source
+# And calculates the top nouns for each article in that news source
+
+def top_proper_nouns_per_article(collection, nlp, prop_noun_threshold=0.1):
+    collection_noun_choices = []
+
+    # Loop over all the articles in the news source
+    for doc in collection:
+
+        # We extract all the proper nouns from the article using spacy's library
+        processed_doc = nlp(doc)
+        all_prop_nouns = [token.lemma_ for token in processed_doc if token.pos_ == "PROPN"]
+        prop_noun_count = len(all_prop_nouns)
+        
+        # Next, we count up how many times each proper noun appeared in the article
+        noun_counts = dict()
+
+        for noun in all_prop_nouns:
+            if noun in noun_counts:
+                noun_counts[noun] += 1
+            else:
+                noun_counts[noun] = 1
+
+        # Finally, we filter out the nouns that appeared less than a minimum threshold
+        final_noun_choices = [k for k,v in noun_counts.items() if (v / prop_noun_count) > prop_noun_threshold]
+
+        collection_noun_choices.append(final_noun_choices)
+
+    # We return the list of documents along with the nouns we selected for each document
+    return list(zip(collection, collection_noun_choices))
+
+# This function was mostly copied from https://www.activestate.com/blog/how-to-do-text-summarization-with-python/
+# It extracts the sentences from a text that contain the most important keywords in that text
+def extraction_summarize(text, target_length, nlp):
+    doc = nlp(text)
+    word_frequencies = {}
+    for word in doc:
+        if word.text.lower() not in list(STOP_WORDS):
+            if word.text.lower() not in updated_punctuation:
+                if word.text not in word_frequencies.keys():
+                    word_frequencies[word.text] = 1
+                else:
+                    word_frequencies[word.text] += 1
+    max_frequency = max(word_frequencies.values())
+    for word in word_frequencies.keys():
+        word_frequencies[word] = word_frequencies[word] / max_frequency
+    sentence_tokens = [sent for sent in doc.sents]
+    sentence_scores = {}
+    for sent in sentence_tokens:
+        for word in sent:
+            if word.text.lower() in word_frequencies.keys():
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word.text.lower()]
+                else:
+                    sentence_scores[sent] += word_frequencies[word.text.lower()]
+    sentence_tuples = [(sentence, score) for sentence, score in sentence_scores.items()]
+    sentence_tuples.sort(key = lambda x: x[1], reverse=True)
+    summary = ""
+
+    sentence_number = 0
+
+    while (len(summary.split()) < target_length and sentence_number < len(sentence_tuples)):
+        summary += sentence_tuples[sentence_number][0].text
+        sentence_number += 1
+        
+    return summary
+
+# This function performs abstraction summarization on a given text, using neural networks to 
+# write a whole new summary for the given text.
+def abstraction_summarize(original_text):
+    summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+    summary_text = summarizer(original_text, min_length = 100)
+    return summary_text[0]['summary_text']
 
 # This function returns whether or not a particular tag is visible to the viewer of a web page
 def tag_visible(element):
@@ -18,34 +111,33 @@ def text_from_html(body):
     visible_texts = filter(tag_visible, texts)
     return u" ".join(t.get_text() for t in visible_texts)
 
-# This is a list of the RSS feeds we are subscribing to for security news
-newsSources = {
-    'The Hacker News': 'https://feeds.feedburner.com/TheHackersNews?format=xml',
-    'Graham Cluley': 'https://www.grahamcluley.com/feed/',
-    'Krebs on Security': 'http://krebsonsecurity.com/feed/',
-    'Threatpost': 'https://threatpost.com/feed/',
-    'Naked Security': 'https://nakedsecurity.sophos.com/feed/'
-}
-
-articleTexts = []
-
-for title, source in newsSources.items():
-    # For every news source, we get the RSS feed. 
-    feed = feedparser.parse(source)
-
-    for article in feed['items']:
-        #For every article in the feed, we open the web page
-        html = urllib.request.urlopen(article['link']).read()
-
-        #Then we extract the text from the web page and put it in an object
-        articleTexts.append({
-            "title" : article['title'],
-            "body" : text_from_html(html)
-        })
-        break # Remove to loop through all sources; currently we just get one article from each source
+# This function defines a progress bar that we can use to show how far along we are in the process
+# It was taken from https://stackoverflow.com/questions/3173320/text-progress-bar-in-terminal-with-block-characters
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
 
 
-# This secion outputs all of the articles in a nice format
+############################## MAIN PROGRAM #################################
+
 print("""
   _____     __             _  __             
  / ___/_ __/ /  ___ ____  / |/ /__ _    _____
@@ -56,8 +148,110 @@ print("""
 
 The Latest in Cybersecurity News!
 """)
-for article in articleTexts:
-    print("---------------------------------")
-    print(article['title'])
-    print("---")
-    print(article['body'])
+
+# This is a list of the RSS feeds we are subscribing to for security news
+newsSources = {
+    'The Hacker News': 'https://feeds.feedburner.com/TheHackersNews?format=xml',
+    #'Graham Cluley': 'https://www.grahamcluley.com/feed/',
+    'Krebs on Security': 'http://krebsonsecurity.com/feed/',
+    'Threatpost': 'https://threatpost.com/feed/',
+    'Naked Security': 'https://nakedsecurity.sophos.com/feed/',
+    'Davinci Forensics': 'https://davinciforensics.co.za/cybersecurity/feed/',
+    'Tech Republic': 'https://www.techrepublic.com/rssfeeds/topic/security/?feedType=rssfeeds',
+    'Computer World': 'https://www.computerworld.com/uk/category/security/index.rss'
+}
+
+source_articles = dict()
+
+for title, source in newsSources.items():
+
+    articleTexts = []
+
+    # For every news source, we get the RSS feed. 
+    feed = feedparser.parse(source)
+
+    # Progress bar for each news source
+    l = len(feed['items'])
+    printProgressBar(0, l, prefix = 'Getting news source: ' + title, suffix = 'Complete', length = 50)
+
+    for i, article in enumerate(feed['items']):
+
+        #For every article in the feed, we open the web page
+        html = urllib.request.urlopen(article['link']).read()
+
+        #Then we extract the text from the web page and put it in an object
+        articleTexts.append(text_from_html(html))
+
+        printProgressBar(i + 1, l, prefix = 'Getting news source: ' + title, suffix = 'Complete', length = 50)
+
+    source_articles[source] = articleTexts
+
+
+# Load the natural language processing library beforehand so we don't have to load it each time we make a call to it
+nlp = spacy.load('en_core_web_sm')
+
+all_sources_proper_noun_counts = dict()
+
+# Progress bar for proper noun extraction
+l = len(source_articles)
+i = 0
+printProgressBar(0, l, prefix = 'Extracting Proper Nouns', suffix = 'Complete', length = 50)
+
+# For every source, we extract the proper nouns from its articles and add their counts to a dictionary
+for source, articles in source_articles.items():
+
+    i += 1
+    printProgressBar(i, l, prefix = 'Extracting Proper Nouns', suffix = 'Complete', length = 50)
+    source_proper_noun_counts = top_proper_nouns_per_article(articles, nlp=nlp)
+
+    all_sources_proper_noun_counts[source] = source_proper_noun_counts
+
+# Here we calculate the top proper nouns across all the different news sources
+all_proper_nouns = {}
+
+for source, articles in all_sources_proper_noun_counts.items():
+    for article_text, noun_choices in articles:
+        for noun in noun_choices:
+            if noun in all_proper_nouns:
+                all_proper_nouns[noun] += 1
+            else:
+                all_proper_nouns[noun] = 1
+
+sorted_proper_nouns = sorted(all_proper_nouns.items(), key=lambda x: x[1], reverse=True)
+
+# We should only take articles that appear in multiple news sources
+top_article_nouns = [noun for noun, count in all_proper_nouns.items() if count > 1]
+top_article_nouns = top_article_nouns[:10]
+print("Topics to summarize are: ", top_article_nouns)
+
+# Now let's get all the articles that contain each top proper noun
+top_articles = dict()
+all_articles = []
+for articles in all_sources_proper_noun_counts.values():
+    all_articles.extend(articles)
+for noun in top_article_nouns:
+    top_articles[noun] = [article_text for article_text, noun_choices in all_articles if noun in noun_choices]
+
+all_summaries = {}
+
+# Progress bar for topic summarization
+l = 10
+i = 0
+printProgressBar(0, l, prefix = 'Summarizing News', suffix = 'Complete', length = 50)
+
+# Finally, we summarize all the articles that belong to a particular topic
+for topic, articles in top_articles.items():
+    full_text = ""
+    for article in articles:
+        full_text += article + " "
+    
+    i += 1
+    printProgressBar(i, l, prefix = 'Summarizing News', suffix = 'Complete', length = 50)
+    all_summaries[topic] = abstraction_summarize(extraction_summarize(full_text, 500, nlp))
+
+# This section outputs all of the summaries in a nice format
+
+for topic, summary in all_summaries.items():
+    print("\n---------------\n")
+    print("Topic:", topic, "\n")
+    print(summary)
